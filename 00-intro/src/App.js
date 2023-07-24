effccc9de51b53bd98ba3608ca48ce070c0d6cdf
AgentKazy <kazygaming@outlook.pt>
2023-07-24 12:47:08 +0100

    10. React vs. Vanilla JavaScript

--- 00-intro/src/App.js
+++ 00-intro/src/App.js
@@ -4,6 +4,13 @@ export default function App() {
   const [advice, setAdvice] = useState('');
   const [count, setCount] = useState(0);
 
+  /* const getAdvice = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }; */
+
   async function getAdvice() {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
