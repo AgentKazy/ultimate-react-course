ed305ee132d246ad4a50988cb3dec39185cd43bc
AgentKazy <kazygaming@outlook.pt>
2023-07-30 21:22:41 +0100

    39. JavaScript Logic in Components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -74,9 +74,23 @@ const Menu = () => {
 };
 
 const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 21;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert("We're currently open!");
+  // } else {
+  //   alert("Sorry, we're closed!");
+  // }
+
   return (
     <footer>{new Date().toLocaleTimeString()}We're currently open!</footer>
   );
+
+  // return React.createElement('footer', null, "We're currently open!");
 };
 
 function Pizza() {
