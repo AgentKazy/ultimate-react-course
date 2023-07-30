63e47f7bc1a3a5eed3b6cb09efc74de749251ae7
AgentKazy <kazygaming@outlook.pt>
2023-07-30 17:03:15 +0100

    38. Creating More Components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -50,13 +50,34 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+const Header = () => {
+  return <h1>Fast React Pizza Co.</h1>;
+};
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
-}
+};
+
+const Footer = () => {
+  return (
+    <footer>{new Date().toLocaleTimeString()}We're currently open!</footer>
+  );
+};
 
 function Pizza() {
   return (
