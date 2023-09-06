c5520025567ad70f64249d13558f77cfe85c3887
AgentKazy <kazygaming@outlook.pt>
2023-09-06 12:42:30 +0100

    148. The useEffect Dependency Array

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -58,7 +58,7 @@ export default function App() {
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  const query = "afdgwe4g";
+  const query = "Hobbit";
 
   useEffect(function () {
     async function fetchMovies() {
