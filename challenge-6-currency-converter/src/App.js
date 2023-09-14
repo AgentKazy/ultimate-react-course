d38e096fb15c3bb3d5a3b7d0f310f410e3ae851c
AgentKazy <kazygaming@outlook.pt>
2023-09-14 01:43:50 +0100

    158. CHALLENGE: Currency Converter

---
+++ challenge-6-currency-converter/src/App.js
@@ -0,0 +1,63 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [inputCurrency, setInputCurrency] = useState("EUR");
+  const [outputCurrency, setOutputCurrency] = useState("USD");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${inputCurrency}&to=${outputCurrency}`
+        );
+        const data = await res.json();
+        setConverted(data.rates[outputCurrency]);
+        setIsLoading(false);
+      }
+
+      if (inputCurrency === outputCurrency) return setConverted(amount);
+      convert();
+    },
+    [amount, inputCurrency, outputCurrency]
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={inputCurrency}
+        onChange={(e) => setInputCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={outputCurrency}
+        onChange={(e) => setOutputCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        {converted} {outputCurrency}
+      </p>
+    </div>
+  );
+}
