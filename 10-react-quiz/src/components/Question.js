eff1350b114d52f54e44d19ece76e2bfe3fedf89
AgentKazy <kazygaming@outlook.pt>
2023-09-17 13:24:22 +0100

    194. Displaying Questions

---
+++ 10-react-quiz/src/components/Question.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Option from "./Option";
+
+export default function Question({ question }) {
+  console.log(question);
+
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Option question={question} />
+    </div>
+  );
+}
