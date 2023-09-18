c1f5ed91d3ce70f043ed4b87483696a05d98d0f7
AgentKazy <kazygaming@outlook.pt>
2023-09-18 12:45:40 +0100

    199. Restarting a Quiz

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -49,6 +49,13 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        highscore: state.highscore,
+        status: "ready",
+      };
 
     default:
       throw new Error("Action unknown");
@@ -109,6 +116,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
