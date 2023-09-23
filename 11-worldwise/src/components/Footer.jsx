61f422dfd796abaf83f84c7edefd7008a1034377
AgentKazy <kazygaming@outlook.pt>
2023-09-23 12:18:59 +0100

    212. Nested Routes and Index Route

--- 11-worldwise/src/components/Footer.jsx
+++ 11-worldwise/src/components/Footer.jsx
@@ -2,12 +2,10 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <div>
-      <footer className={styles.footer}>
-        <p className={styles.copyright}>
-          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
-        </p>
-      </footer>
-    </div>
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>
+        &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+      </p>
+    </footer>
   );
 }
