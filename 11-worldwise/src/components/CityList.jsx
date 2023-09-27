fe202ffffa753b032ab90d1c0b3aec95bbac62b1
AgentKazy <kazygaming@outlook.pt>
2023-09-27 10:06:28 +0100

    228. Consuming the CitiesContext

--- 11-worldwise/src/components/CityList.jsx
+++ 11-worldwise/src/components/CityList.jsx
@@ -3,8 +3,10 @@ import CityItem from "./CityItem";
 import Message from "./Message";
 
 import styles from "./CityList.module.css";
+import { useCities } from "../context/CitiesContext";
 
-export default function CityList({ cities, isLoading }) {
+export default function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
--- 11-worldwise/src/components/CountryList.jsx
+++ 11-worldwise/src/components/CountryList.jsx
@@ -3,8 +3,10 @@ import CountryItem from "./CountryItem";
 import Message from "./Message";
 
 import styles from "./CountryList.module.css";
+import { useCities } from "../context/CitiesContext";
 
-export default function CountryList({ cities, isLoading }) {
+export default function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
