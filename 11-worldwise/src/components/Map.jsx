4988990c8b42c0b564a4109ae286d4acfc91ef8b
AgentKazy <kazygaming@outlook.pt>
2023-09-28 12:37:54 +0100

    234. Fetching City Data in the Form

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -14,20 +14,19 @@ import { useGeolocation } from "../hooks/useGeolocation";
 import Button from "./Button";
 
 import styles from "./Map.module.css";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
   const { cities } = useCities();
 
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams, setSearchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
