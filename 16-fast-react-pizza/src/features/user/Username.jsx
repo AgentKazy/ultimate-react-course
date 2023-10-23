d31554cd59ec4427174f04fbd2ff81fa943587f6
AgentKazy <kazygaming@outlook.pt>
2023-10-23 12:44:44 +0100

    298. The Box Model: Spacing, Borders, and Display

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Bruno</div>;
+  return <div className="hidden text-sm font-semibold">Bruno</div>;
 }
 
 export default Username;
