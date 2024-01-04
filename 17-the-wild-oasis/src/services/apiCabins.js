6cfd2025bb4a29715db11627737c881d60ae3c1c
AgentKazy <kazygaming@outlook.pt>
2024-01-04 23:02:22 +0000

    355. Uploading Images to Supabase

--- 17-the-wild-oasis/src/services/apiCabins.js
+++ 17-the-wild-oasis/src/services/apiCabins.js
@@ -12,14 +12,39 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${
+    import.meta.env.VITE_SUPABASEURL
+  }/storage/v1/object/public/cabin-images/${imageName}`;
+
+  //https://ucpdghenhezuknlavciz.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+
+  // 1. Create cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.log(error);
     throw new Error("Cabin could not be created.");
+  }
+
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin IF there was an error uploading the corresponding image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded. Cabin was not created."
+    );
   }
 
   return data;
