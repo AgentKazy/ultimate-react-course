49ab6c3eb8661726a04a2d2c44069632b10b92b9
AgentKazy <kazygaming@outlook.pt>
2024-01-08 14:56:59 +0000

    376. Client-Side Sorting: Sorting Cabins

--- 17-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
@@ -1,4 +1,5 @@
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 import TableOperations from "../../ui/TableOperations";
 
 function CabinTableOperations() {
@@ -10,6 +11,29 @@ function CabinTableOperations() {
           { value: "all", label: "All" },
           { value: "no-discount", label: "No discount" },
           { value: "with-discount", label: "With discount" },
+        ]}
+      />
+
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-desc", label: "Sort by name (Z-A)" },
+          {
+            value: "regularPrice-asc",
+            label: "Sort by price (\u2191)",
+          },
+          {
+            value: "regularPrice-desc",
+            label: "Sort by price (\u2193)",
+          },
+          {
+            value: "maxCapacity-asc",
+            label: "Sort by capacity (\u2191)",
+          },
+          {
+            value: "maxCapacity-desc",
+            label: "Sort by capacity (\u2193)",
+          },
         ]}
       />
     </TableOperations>
