066ae94b7c78bbbd7855d085114dc4bc7168cfae
AgentKazy <kazygaming@outlook.pt>
2024-01-09 21:46:33 +0000

    379. API-Side Filtering: Filtering Bookings

--- 17-the-wild-oasis/src/features/bookings/BookingTableOperations.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingTableOperations.jsx
@@ -17,13 +17,13 @@ function BookingTableOperations() {
 
       <SortBy
         options={[
-          { value: "startDate-desc", label: "Sort by date (recent first)" },
-          { value: "startDate-asc", label: "Sort by date (earlier first)" },
+          { value: "startDate-asc", label: "Sort by date (\u2191)" },
+          { value: "startDate-desc", label: "Sort by date (\u2193)" },
+          { value: "totalPrice-asc", label: "Sort by amount (\u2191)" },
           {
             value: "totalPrice-desc",
-            label: "Sort by amount (high first)",
+            label: "Sort by amount (\u2193)",
           },
-          { value: "totalPrice-asc", label: "Sort by amount (low first)" },
         ]}
       />
     </TableOperations>
